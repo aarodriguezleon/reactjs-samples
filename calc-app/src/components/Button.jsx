7f284c6e345e5278f15951f3ae5e8db9e12b6c8f
Angel Rodriguez <angel.rodriguez.leon@gmail.com>
2022-07-08 07:47:16 -0300

    console log

--- calc-app/src/components/Button.jsx
+++ calc-app/src/components/Button.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Button = ({type, text, onclickHandler}) => {
     return (
         <button className={type} onClick={()=>{
+            console.log("Click en el Botton")
             onclickHandler(text)
         }}>
             <span>{text}</span>
